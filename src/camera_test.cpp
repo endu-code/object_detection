4fbdc3b4492e4cd002017cd5d09f58477192745d
Dario <dario.aeschlimann@paoluzzo.ch>
2021-10-22 16:24:23 +0200

    fixed camera test

--- src/camera_test.cpp
+++ src/camera_test.cpp
@@ -16,7 +16,17 @@ void MyLine(Mat img, Point start, Point end) {
 
     line(img, start, end, Scalar(0, 0, 0), thickness, lineType);
     }
+void camStream(int deviceID){
+    VideoCapture capture(deviceID);
+    Mat captureFrame;
 
+    if (!capture.open(deviceID)){
+        return;
+    }
+
+    capture >> captureFrame;
+    
+}
 int main(int argc, char** argv)
     {
     VideoCapture cap(2);
@@ -24,13 +34,13 @@ int main(int argc, char** argv)
     // Check VideoCapture documentation.
     if (!cap.open(2))
         return 0;
-    
+
     // Load Test Image for shape detection
     //Mat TestImage = imread("/home/dario/catkin_ws/src/endu_robotics/sample/Test.png");
 
     // Load sample images and store in Array
-    Mat shapes[3], shapes_bin[3], detected_edges, shape_edges[3], drawn_contours[3];
-    std::vector<std::vector<Point>> contours, shape_contours[3];
+    Mat shapes[3], shapes_bin[3], detected_edges, shape_edges[3], drawn_contours[3], drawnEdges;
+    std::vector<std::vector<Point>> contours, shape_contours[3], contours_filtered;
     std::vector<Vec4i> hierarchy, shape_hierarchy[3];
     Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
     double result[3];
@@ -46,75 +56,60 @@ int main(int argc, char** argv)
         blur(shapes_bin[i], shapes_bin[i], Size(3, 3));
         Canny(shapes_bin[i], shape_edges[i], 0, 0, 3);
         findContours(shape_edges[i], shape_contours[i], shape_hierarchy[i], RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-        ROS_INFO_STREAM("Vector size of shape_contours[" << i << "]" << shape_contours[i].size());
-        drawn_contours[i] = Mat::zeros(shape_edges[i].size(), CV_8UC3);
+       drawn_contours[i] = Mat::zeros(shape_edges[i].size(), CV_8UC3);
         for (size_t j = 0; j < shape_contours[i].size(); j++) {
             color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
             drawContours(drawn_contours[i], shape_contours[i], (int)j, color, 2, LINE_8, shape_hierarchy[i], 0);
             }
         }
 
-    // Show thresholded samples
-    // imshow("cloud1", drawn_contours[0]);
-    // imshow("cross1", drawn_contours[1]);
-    // imshow("straight1", drawn_contours[2]);
-
-    while(1)
+    while (1)
         {
         Mat frame, frame_gray, frame_bin;
         cap >> frame;
         //frame = TestImage;
+
         cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
         threshold(frame_gray, frame_bin, 175, 255, 1);
-
         blur(frame_bin, frame_bin, Size(3, 3));
         Canny(frame_bin, detected_edges, 0, 0, 3);
 
         findContours(detected_edges, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-        Mat drawnEdges = Mat::zeros(detected_edges.size(), CV_8UC3);
-
-
-        // shape 0 == cloud
-        // shape 1 == cross
-        // shape 2 == straight 
+        drawnEdges = Mat::zeros(detected_edges.size(), CV_8UC3);
 
         for (size_t i = 0; i < contours.size(); i++) {
-            
-            color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+           if (contourArea(contours.at(i)) > 2000 && contourArea(contours.at(i)) < 10000 ) {
+                color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+                result[0] = matchShapes(contours[i], shape_contours[0][0], 1, 0);
+                result[1] = matchShapes(contours[i], shape_contours[1][0], 1, 0);
+                result[2] = matchShapes(contours[i], shape_contours[2][0], 1, 0);
 
-            result[0] = matchShapes(contours[i], shape_contours[0][0], 1, 0);
-            ROS_INFO_STREAM("Matching Against Cloud: " << result[0]);
-            result[1] = matchShapes(contours[i], shape_contours[1][0], 1, 0);
-            ROS_INFO_STREAM("Matching Against Cross: " << result[1]);
-            result[2] = matchShapes(contours[i], shape_contours[2][0], 1, 0);
-            ROS_INFO_STREAM("Matching Against Straight: " << result[3]);
-            
-            if (result[0] == std::min(result[0], (std::min(result[1], result[2])))) {
-                if (result[0] < 0.05) {
-                    drawContours(drawnEdges, contours, (int)i, color, 2, LINE_8, hierarchy, 0);
-                    putText(drawnEdges, "cloud", contours.at(i).at(0), 1, 2, color);
+                if (result[0] == std::min(result[0], (std::min(result[1], result[2])))) {
+                    if (result[0] < 0.01) {
+                        drawContours(drawnEdges, contours, (int)i, color, 2, LINE_8, hierarchy, 0);
+                        putText(drawnEdges, "cloud", contours.at(i).at(0), 1, 2, color);
+                        }
                     }
-                }
-            else if (result[1] == std::min(result[1], (std::min(result[0], result[2])))) {
-                if (result[1] < 0.05) {
-                    drawContours(drawnEdges, contours, (int)i, color, 2, LINE_8, hierarchy, 0);
-                    putText(drawnEdges, "cross", contours.at(i).at(0), 1, 2, color);
+                else if (result[1] == std::min(result[1], (std::min(result[0], result[2])))) {
+                    if (result[1] < 0.01) {
+                        drawContours(drawnEdges, contours, (int)i, color, 2, LINE_8, hierarchy, 0);
+                        putText(drawnEdges, "cross", contours.at(i).at(0), 1, 2, color);
+                        }
                     }
-                }
-            else if (result[2] == std::min(result[2], (std::min(result[1], result[0])))) {
-                if (result[2] < 0.05) {
-                    drawContours(drawnEdges, contours, (int)i, color, 2, LINE_8, hierarchy, 0);
-                    putText(drawnEdges, "straight", contours.at(i).at(0), 1, 2, color);
+                else if (result[2] == std::min(result[2], (std::min(result[1], result[0])))) {
+                    if (result[2] < 0.01) {
+                        drawContours(drawnEdges, contours, (int)i, color, 2, LINE_8, hierarchy, 0);
+                        putText(drawnEdges, "straight", contours.at(i).at(0), 1, 2, color);
+                        }
                     }
-                }
 
-            if (frame.empty()) break; // end of video stream
-            // imshow("dst", dst);
-            imshow("frame", frame);
-            imshow("frame_bin", frame_bin);
-            // imshow("detected_edges", detected_edges);
-            imshow("drawnCountours", drawnEdges);
-            if (waitKey(10) == 27) break; // stop capturing by pressing ESC 
+                if (frame.empty()) break; // end of video stream
+                imshow("frame", frame);
+                imshow("frame_bin", frame_bin);
+                // imshow("detected_edges", detected_edges);
+                imshow("drawnCountours", drawnEdges);
+                if (waitKey(10) == 27) break; // stop capturing by pressing ESC 
+                }
             }
         }
     return 0;
